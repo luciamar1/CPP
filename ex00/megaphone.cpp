4267c1309aff5427d89145bfe0e6602aa32e7a26
luciamar1 <luciajimenamartinrodriguez@gmail.com>
2024-04-16 21:07:17 +0200

    primer 00 00 =)

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+static void print_upper(const char *str)
+{
+    std::string aux(str);
+    std::iterator counter = 0;
+    for(it != std::aux.end; i++)
+        std::cout << (char)std::toupper(aux[counter]);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+    else
+        for (std::iterator counter = 1; counter <= argc; counter ++)
+            print_upper(argv[counter]);
+    std::cout << std::endl;
+    return(0);
+}
