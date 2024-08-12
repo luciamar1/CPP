a7209418b0f35e264b86bddc742c90dbebf41968
lucia-ma <hola.tuttttiiiii.24@gmail.com>
2024-08-12 18:43:01 +0200

    ex00 cpp07 hecho

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -4,26 +4,8 @@
 template <typename T>
 void    iter(T &a, T &b, T &c)
 {
-    T aux; 
-    aux = a;
-    a = b;
-    b = aux;
+
 }
 
-template <typename T>
-T min( T const &a,  T const &b)
-{
-    if (a>=b)
-        return b;
-    return a;
-}
-
-template <typename T>
-T max( T const &a,  T const &b)
-{
-    if (a<=b)
-        return b;
-    return a;
-}
 
 #endif
