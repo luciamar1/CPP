e5e514eeb7743e6760f2dc85ea70d6c6e8aec88e
lucia-ma <hola.tuttttiiiii.24@gmail.com>
2024-09-17 20:09:41 +0200

    primer dia

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,23 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template<typename T>
+class Array
+{
+private:
+    T   element;
+public:
+    Array(/* args */);
+    ~Array();
+};
+
+Array::Array(/* args */)
+{
+}
+
+Array::~Array()
+{
+}
+
+
+#endif
