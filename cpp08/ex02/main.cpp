cb252c49657d2ee3db3cf11c8ae2192f354811ea
lucia-ma <hola.tuttttiiiii.24@gmail.com>
2024-10-01 18:29:11 +0200

    cpp08

---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "MutantStack.h"
+
+int main()
+{
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << "Top element: " << mstack.top() << std::endl;
+    
+    mstack.pop();
+    std::cout << "Stack size after pop: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+
+    std::cout << "Stack elements:" << std::endl;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    std::stack<int> s(mstack); // Copiar el MutantStack en un std::stack
+
+    return 0;
+}
