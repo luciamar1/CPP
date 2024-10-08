690e61a8f792f774bb59742a0f9245829f50784b
lucia-ma <hola.tuttttiiiii.24@gmail.com>
2024-10-08 15:28:38 +0200

    cpp01 bien

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -11,7 +11,6 @@ std::string FileIntoStr(std::ifstream& file_0)
         return_file.append(str_file);
         return_file.append("\n");
     }
-    std::cout << return_file << std::endl;
     return (return_file);
 }
 
@@ -33,9 +32,7 @@ std::string replaceFile(std::string& str_file, const std::string str1, const std
         {
 
             result.append(str_file, counter, pos-counter);
-            std::cout << "1       " << result << std::endl;
             result.append(str2);
-            std::cout << "2        " << result << std::endl;
             counter = pos + str1.length();
         }
         
@@ -48,23 +45,33 @@ std::string replaceFile(std::string& str_file, const std::string str1, const std
 int main(int argc, char **argv)
 {
     if(argc != 4 )
-        return(0);
-    else
     {
-        std::ifstream file_0(argv[1]);
-        if (!file_0.is_open())
-            return(1);
-        std::string str_file = FileIntoStr(file_0);
-        str_file = replaceFile(str_file, argv[2], argv[3]);
-        
-        std::ofstream return_file("return_file");
-        if (!return_file.is_open())
-        {
-            std::cerr << "Error: no se pudo crear el archivo " << "return_file" << std::endl;
-            return (1);
-        }
-        return_file << str_file;
-        return_file.close();
+        std::cerr << "Error: missing params "  << std::endl;
+        return(0);
     }
+
+    std::ifstream file_0(argv[1]);
+    if (!file_0.is_open())
+    {
+        std::cerr << "Error:  Unable to open file "  <<  argv[1] << std::endl;
+        return(1);
+    }
+
+    std::string str_file = FileIntoStr(file_0);
+    file_0.close();
+    
+    str_file = replaceFile(str_file, argv[2], argv[3]);
+    
+
+    std::string output_filename = std::string(argv[1]) +".replace";
+    std::ofstream return_file(output_filename.c_str());
+    if (!return_file.is_open())
+    {
+        std::cerr << "Error: file could not be created " << "return_file" << std::endl;
+        return (1);
+    }
+    return_file << str_file;
+    return_file.close();
+    std::cout << "Successfully created " << output_filename << std::endl; 
     return(0);
 }
